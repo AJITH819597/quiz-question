05eb8f7d3d85253bcef9324e43e7390e1b2c36fd
AJITH819597 <50359191+AJITH819597@users.noreply.github.com>
2020-09-23 09:43:31 -0700

    initial commit

---
+++ build/precache-manifest.527fb6b75ed246e758b70ba5c9098529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0234a65f5fef40d9b393df6752eb25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "201fe730edb5aca3a47a",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "431e44eca0f0169fbbc7",
+    "url": "/static/js/2.9f25e938.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f25e938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201fe730edb5aca3a47a",
+    "url": "/static/js/main.d97e4d0d.chunk.js"
+  },
+  {
+    "revision": "44bfa817bc26e1d657ac",
+    "url": "/static/js/runtime-main.14514217.js"
+  }
+]);
